d77997924b081b142c983a88cd8474c669c0b109
Cainã Costa <me@cfcosta.com>
2022-08-22 09:05:20 -0300

    fix[scripts]: fail if apply script is not run as root

--- apply.sh
+++ apply.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-#
+
+set -e
+
 ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+[ "$(whoami)" == "root" ] || (
+    echo "This script needs to be run as root."
+    exit 1
+)
 
 nixos-rebuild switch --flake "${ROOT}#$(hostname)"
