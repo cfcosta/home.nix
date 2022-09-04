7f9e6b90bb4036bba32ee223d38c5afbf5072c81
Cainã Costa <me@cfcosta.com>
2022-09-04 19:21:07 -0300

    Revert "fix: move back to normal emacs for less weirdnesses"
    This reverts commit a8eb74fad2e3a13942d20b6450ff2d66e8df6236.

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -54,7 +54,7 @@ in {
     programs.doom-emacs = {
       enable = true;
       doomPrivateDir = ../../templates/doom-emacs;
-      emacsPackage = pkgs.emacs;
+      emacsPackage = pkgs.emacsNativeComp;
     };
 
     home.packages = with pkgs; [ nodePackages.mermaid-cli ];
