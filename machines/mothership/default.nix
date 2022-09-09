14be716f2626f4252123d045e36cdd3aedecb3ee
Cainã Costa <me@cfcosta.com>
2022-09-09 11:23:18 -0300

    mothership: set default substituters

--- machines/mothership/default.nix
+++ machines/mothership/default.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }: {
   imports = [ ./hardware-configuration.nix ];
 
+  nix.settings.substituters =
+    [ "https://cfcosta-home.cachix.org" "https://cache.nixos.org" ];
+
+  nix.settings.trusted-public-keys = [
+    "cfcosta-home.cachix.org-1:Ly4J9QkKf/WGbnap33TG0o5mG5Sa/rcKQczLbH6G66I="
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
