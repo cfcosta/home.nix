745bac5000dcefa5c83ba81983534f69a3248d3e
Cainã Costa <me@cfcosta.com>
2022-09-11 17:12:18 -0300

    emacs: disable emoji

--- templates/doom-emacs/init.el
+++ templates/doom-emacs/init.el
@@ -9,7 +9,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)
+       ;;emoji
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;(ligatures +extra)         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
