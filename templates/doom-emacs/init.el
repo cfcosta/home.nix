ebcc7e63dba5c39ec51b08ace002e3d5cf02e54e
Cainã Costa <me@cfcosta.com>
2022-10-18 15:48:23 -0300

    feat[emacs]: enable javascript and typescript

--- templates/doom-emacs/init.el
+++ templates/doom-emacs/init.el
@@ -109,7 +109,7 @@
        ;;idris             ; a language you can depend on
        (json +lsp +tree-sitter)
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
