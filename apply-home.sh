e305a15167e57650f0cc90f05ef774cb6d7d7529
Cainã Costa <me@cfcosta.com>
2023-03-18 18:46:14 -0300

    fix: make home-manager activation more reliable
    In some weird cases, `home-manager switch --flake` failed to find the
    activation script target, so we now build it directly and then activate.

--- apply-home.sh
+++ apply-home.sh
@@ -11,5 +11,8 @@ get_system() {
 ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 ARCH="$(uname -m)"
 SYSTEM="$(get_system)"
+NIX_FLAGS="--extra-experimental-features nix-command --extra-experimental-features flakes"
 
-home-manager switch --flake "${ROOT}#${SYSTEM}.$(whoami)@$(hostname)"
+nix build ${NIX_FLAGS} "${ROOT}#homeConfigurations.${SYSTEM}.$(whoami)@$(hostname).activation-script"
+
+exec "${ROOT}/result/activate"
