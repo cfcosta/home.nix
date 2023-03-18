bdef8fa2d94fefa786797b22fd49fe550aea04c7
Cainã Costa <me@cfcosta.com>
2023-03-18 20:23:36 -0300

    feat: adapt apply script to new paths
    This forgoes the use of `nixos-rebuild switch` to instead use nix build
    directly. This is in preparation to start supporting remote deployment
    of the config.

--- apply.sh
+++ apply.sh
@@ -2,11 +2,30 @@
 
 set -e
 
-ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
 [ "$(whoami)" == "root" ] || (
     echo "This script needs to be run as root."
     exit 1
 )
 
-nixos-rebuild switch --flake "${ROOT}#$(hostname)"
+get_arch() {
+  case "$(uname -m)" in
+    "arm64") echo "aarch64";;
+    *) echo "x86_64";;
+  esac
+}
+
+get_system() {
+  case "$(uname)" in
+    "Linux") echo "linux";;
+    *) echo "darwin";;
+  esac
+}
+
+ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SYSTEM="$(get_arch)-$(get_system)"
+
+nix build \
+  --extra-experimental-features nix-command --extra-experimental-features flakes \
+ "${ROOT}#profiles.${SYSTEM}.$(hostname -s).system.config.system.build.toplevel"
+
+exec "${ROOT}/result/bin/switch-to-configuration" switch
