579f53ac9fa71767fe1604afdb81b0ba23206d5e
Cainã Costa <me@cfcosta.com>
2023-05-26 12:48:39 -0300

    chore: format shell files

--- apply-home.sh
+++ apply-home.sh
@@ -3,24 +3,24 @@
 set -e
 
 get_arch() {
-  case "$(uname -m)" in
-    "arm64") echo "aarch64";;
-    *) echo "x86_64";;
-  esac
+	case "$(uname -m)" in
+	"arm64") echo "aarch64" ;;
+	*) echo "x86_64" ;;
+	esac
 }
 
 get_system() {
-  case "$(uname)" in
-    "Linux") echo "linux";;
-    *) echo "darwin";;
-  esac
+	case "$(uname)" in
+	"Linux") echo "linux" ;;
+	*) echo "darwin" ;;
+	esac
 }
 
-ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 SYSTEM="$(get_arch)-$(get_system)"
 
 nix build \
-  --extra-experimental-features nix-command --extra-experimental-features flakes \
-  "${ROOT}#profiles.${SYSTEM}.$(hostname -s).home.activation-script"
+	--extra-experimental-features nix-command --extra-experimental-features flakes \
+	"${ROOT}#profiles.${SYSTEM}.$(hostname -s).home.activation-script"
 
 exec "${ROOT}/result/activate"
--- apply.sh
+++ apply.sh
@@ -3,10 +3,10 @@
 set -e
 
 [ "$(whoami)" == "root" ] || (
-    echo "This script needs to be run as root."
-    exit 1
+	echo "This script needs to be run as root."
+	exit 1
 )
 
-ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-exec nixos-rebuild switch --flake ${ROOT}#$(hostname)
+exec nixos-rebuild switch --flake "${ROOT}#$(hostname)"
