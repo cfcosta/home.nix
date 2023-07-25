15043b2de01a39a18bb5a3f7f1e8b776af6aeb60
Cainã Costa <me@cfcosta.com>
2023-07-25 10:24:47 -0300

    feat: enable atuin, extract shell into its own file

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -9,6 +9,7 @@ in {
     ./gnome.nix
     ./media.nix
     ./notes.nix
+    ./shell.nix
     ./tmux.nix
   ];
 
@@ -18,17 +19,6 @@ in {
       email = mkOption { type = types.str; };
       username = mkOption { type = types.str; };
       accounts.github = mkOption { type = types.str; };
-
-      shell = {
-        environmentFile = mkOption {
-          type = types.str;
-          default = "${cfg.folders.home}/dusk-env.sh";
-          description = ''
-            A bash file that is loaded by the shell on each run.
-            This is used to set secrets or credentials that we don't want on the repo.
-          '';
-        };
-      };
 
       folders = {
         code = mkOption {
@@ -59,8 +49,6 @@ in {
     home.sessionVariables.EDITOR = "nvim";
 
     home.packages = with pkgs; [
-      bashInteractive
-      complete-alias
       eva
       fd
       git
@@ -91,70 +79,6 @@ in {
     programs.htop = {
       enable = true;
       package = pkgs.htop-vim;
-    };
-
-    programs.bash = {
-      enable = true;
-
-      shellAliases = {
-        ack = "rg";
-        bc = "eva";
-        cat = "bat --decorations=never";
-        g = "git status --short";
-        gc = "git commit";
-        gca = "git commit -a";
-        gco = "git checkout";
-        gd = "git diff";
-        gf = "git fetch -a";
-        gl = "git log --graph";
-        gp = "git push";
-        gs = "git stash";
-        gsp = "git stash pop";
-        ls = "exa -l";
-        vi = "nvim";
-        vim = "nvim";
-      };
-
-      sessionVariables = {
-        COLORTERM = "truecolor";
-        EDITOR = "nvim";
-        CDPATH = ".:${cfg.folders.code}";
-      };
-
-      historySize = 1000000;
-
-      # Ignore common transaction commands and the aliases we've set.
-      historyIgnore = [ "exit" "pwd" "reset" ]
-        ++ (mapAttrsToList (key: _: key) config.programs.bash.shellAliases);
-
-      shellOptions = [
-        # Default options from home-manager
-        "histappend"
-        "checkwinsize"
-        "extglob"
-        "globstar"
-        "checkjobs"
-
-        # Custom Options
-        "cdspell" # Correct transpositions and other minor details from 'cd DIR' command.
-        "dotglob" # Include hidden files in glob (*).
-        "extglob" # Extra-special pattern matching in the shell.
-        "cmdhist" # Flatten multiple-line commands into the same history entry.
-        "no_empty_cmd_completion" # Don't complete when I haven't typed anything.
-        "shift_verbose" # Let me know if I shift stupidly.
-      ];
-
-      initExtra = ''
-        . ${pkgs.complete-alias}/bin/complete_alias
-
-        ${builtins.concatStringsSep "\n"
-        (builtins.map (alias: "complete -F _complete_alias ${alias}")
-          (builtins.attrNames config.programs.bash.shellAliases))}
-      '';
-
-      bashrcExtra = ''
-        [ -f "${cfg.shell.environmentFile}" ] && source "${cfg.shell.environmentFile}"
-      '';
     };
 
     home.stateVersion = "23.05";
---
+++ modules/home-manager/shell.nix
@@ -0,0 +1,101 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let cfg = config.dusk.home;
+in {
+  options = {
+    dusk.home.shell = {
+      environmentFile = mkOption {
+        type = types.str;
+        default = "${cfg.folders.home}/dusk-env.sh";
+        description = ''
+          A bash file that is loaded by the shell on each run.
+          This is used to set secrets or credentials that we don't want on the repo.
+        '';
+      };
+    };
+  };
+  config = rec {
+    home.packages = with pkgs; [ bashInteractive complete-alias ];
+
+    programs.bash = {
+      enable = true;
+
+      shellAliases = {
+        ack = "rg";
+        bc = "eva";
+        cat = "bat --decorations=never";
+        g = "git status --short";
+        gc = "git commit";
+        gca = "git commit -a";
+        gco = "git checkout";
+        gd = "git diff";
+        gf = "git fetch -a";
+        gl = "git log --graph";
+        gp = "git push";
+        gs = "git stash";
+        gsp = "git stash pop";
+        ls = "exa -l";
+        vi = "nvim";
+        vim = "nvim";
+      };
+
+      sessionVariables = {
+        COLORTERM = "truecolor";
+        EDITOR = "nvim";
+        CDPATH = ".:${cfg.folders.code}";
+      };
+
+      historySize = 1000000;
+
+      # Ignore common transaction commands and the aliases we've set.
+      historyIgnore = [ "exit" "pwd" "reset" ]
+        ++ (mapAttrsToList (key: _: key) config.programs.bash.shellAliases);
+
+      shellOptions = [
+        # Default options from home-manager
+        "histappend"
+        "checkwinsize"
+        "extglob"
+        "globstar"
+        "checkjobs"
+
+        # Custom Options
+        "cdspell" # Correct transpositions and other minor details from 'cd DIR' command.
+        "dotglob" # Include hidden files in glob (*).
+        "extglob" # Extra-special pattern matching in the shell.
+        "cmdhist" # Flatten multiple-line commands into the same history entry.
+        "no_empty_cmd_completion" # Don't complete when I haven't typed anything.
+        "shift_verbose" # Let me know if I shift stupidly.
+      ];
+
+      initExtra = ''
+        . ${pkgs.complete-alias}/bin/complete_alias
+
+        ${builtins.concatStringsSep "\n"
+        (builtins.map (alias: "complete -F _complete_alias ${alias}")
+          (builtins.attrNames config.programs.bash.shellAliases))}
+      '';
+
+      bashrcExtra = ''
+        [ -f "${cfg.shell.environmentFile}" ] && source "${cfg.shell.environmentFile}"
+      '';
+    };
+
+    # Make ctrl-r much more powerful
+    # https://github.com/ellie/atuin
+    programs.atuin = {
+      enable = true;
+      flags = [ "--disable-up-arrow" ];
+      settings = {
+        auto_sync = false;
+        update_check = false;
+        style = "compact";
+        show_help = false;
+
+        history_filter = [ "^ls" "^cd" "^cat" ]
+          ++ (builtins.map (alias: ''"^${alias}"'')
+            (builtins.attrNames config.programs.bash.shellAliases));
+      };
+    };
+  };
+}
