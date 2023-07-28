c83dc01c7476071c238fb78ee2b71a61892ed714
Cainã Costa <me@cfcosta.com>
2023-07-28 12:34:06 -0300

    chore[tmux]: enable powerline for dracula

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -6,7 +6,12 @@
     escapeTime = 0;
     keyMode = "vi";
     terminal = "xterm-256color";
-    plugins = with pkgs.tmuxPlugins; [ dracula ];
+    plugins = with pkgs.tmuxPlugins; [({
+      plugin = dracula;
+      extraConfig = ''
+        set -g @dracula-show-powerline true
+      '';
+    })];
     extraConfig = builtins.readFile ./tmux/config;
   };
 }
