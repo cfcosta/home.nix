e19c3135ebf9de5dceb5df87d7935a6aac9c40ab
Cainã Costa <me@cfcosta.com>
2023-08-22 00:37:19 -0600

    chore: add msmtp and neomutt alias/theme

--- modules/home-manager/mutt.nix
+++ modules/home-manager/mutt.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 with lib;
-let cfg = config.dusk.home;
+let cfg = config.dusk.home.mutt;
 in {
   options = {
     dusk.home.mutt = {
@@ -11,7 +11,16 @@ in {
     };
   };
 
-  config = mkIf cfg.mutt.enable {
-    home.packages = with pkgs; [ offlineimap neomutt ];
+  config = mkIf cfg.enable {
+    home.packages = with pkgs;
+      [ offlineimap neomutt msmtp ]
+      ++ optionals pkgs.stdenv.isLinux [ mailutils ];
+
+    home.file.".mailrc".text = ''
+      set sendmail="${pkgs.msmtp}/bin/msmtp"
+    '';
+    home.file.".muttrc".text = builtins.readFile ./mutt/muttrc;
+
+    programs.bash.shellAliases = { mutt = "neomutt"; };
   };
 }
