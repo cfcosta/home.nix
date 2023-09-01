e44a726a4f554aa7b46ffd252870f64fe3e175e6
Cainã Costa <me@cfcosta.com>
2023-09-01 18:02:39 -0300

    feat(mutt): fix browser handling, save message to todoist

--- modules/home-manager/mutt.nix
+++ modules/home-manager/mutt.nix
@@ -38,19 +38,35 @@ let
       };
     };
   };
-  scripts = {
-    openUrl = pkgs.writeShellScriptBin "open-url"
-      (builtins.readFile ./mutt/mutt-open-url.sh);
+  # Use a fork of todoist that includes a way to insert a description into the generated tasks.
+  todoist = pkgs.buildGoModule rec {
+    pname = "todoist";
+    version = "0.20.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "psethwick";
+      repo = "todoist";
+      rev = "2f80bdc65de44581c4497107a092c73f39ae0b62";
+      sha256 = "sha256-cFnGIJty0c4iOMNeMt+pev75aWcd7HKkvVowd5XbsXs=";
+    };
+    vendorHash = "sha256-fWFFWFVnLtZivlqMRIi6TjvticiKlyXF2Bx9Munos8M=";
+    doCheck = false;
   };
+  scripts = [
+    todoist
+    (pkgs.writeShellScriptBin "mutt-open-url"
+      (builtins.readFile ./mutt/mutt-open-url.sh))
+    (pkgs.writeShellScriptBin "mutt-add-todoist"
+      (builtins.readFile ./mutt/mutt-add-todoist.sh))
+  ];
 in {
   options.dusk.home.mutt.enable = mkEnableOption "mutt";
 
   config = mkIf cfg.enable {
     home.packages = with pkgs;
-      [ abook msmtp neomutt notmuch offlineimap scripts.openUrl urlview ]
+      [ abook msmtp neomutt notmuch offlineimap urlview ] ++ scripts
       ++ optionals pkgs.stdenv.isLinux [ mailutils ];
 
-    home.file.".urlview".text = "COMMAND open-url ";
+    home.file.".urlview".text = "COMMAND mutt-open-url ";
     home.file.".mailrc".text = ''
       set sendmail="${pkgs.msmtp}/bin/msmtp"
     '';
