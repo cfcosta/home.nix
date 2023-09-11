39028f128520246cfe17892a6fb8dbb799c77a29
Cainã Costa <me@cfcosta.com>
2023-09-11 12:10:33 -0300

    chore(direnv): also load .env files if available

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -74,7 +74,13 @@ in {
     programs.go.enable = true;
     programs.jq.enable = true;
     programs.gpg.enable = true;
-    programs.direnv.enable = true;
+
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+
+      config.global.load_dotenv = true;
+    };
 
     programs.bat = {
       enable = true;
