1769c9d4e3155ba96b6c93b94744fdf0ab911e96
Cainã Costa <me@cfcosta.com>
2023-09-11 11:00:16 -0300

    chore(starship): use Dracula theme

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -76,9 +76,30 @@ in {
     programs.btop.enable = true;
     programs.go.enable = true;
     programs.jq.enable = true;
-    programs.starship.enable = true;
     programs.gpg.enable = true;
     programs.direnv.enable = true;
+
+    programs.starship = {
+      enable = true;
+      enableBashIntegration = true;
+
+      settings = {
+        aws.style = "bold #ffb86c";
+        cmd_duration.style = "bold #f1fa8c";
+        directory.style = "bold #50fa7b";
+        hostname.style = "bold #ff5555";
+        git_branch.style = "bold #ff79c6";
+        git_status.style = "bold #ff5555";
+        username = {
+          format = "[$user]($style) on ";
+          style_user = "bold #bd93f9";
+        };
+        character = {
+          success_symbol = "[❯](bold #50fa7b)";
+          error_symbol = "[❯](bold #ff5555)";
+        };
+      };
+    };
 
     programs.htop = {
       enable = true;
