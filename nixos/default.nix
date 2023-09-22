3ff6fd52265eeea968ee1c5647b580807443724c
Cainã Costa <me@cfcosta.com>
2023-09-22 17:21:15 -0300

    chore: make nixos and windows use same time

--- nixos/default.nix
+++ nixos/default.nix
@@ -124,5 +124,8 @@ in {
     services.udev.extraRules = ''
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
     '';
+
+    # Make clock compatible with windows (for dual boot)
+    time.hardwareClockInLocalTime = true;
   };
 }
