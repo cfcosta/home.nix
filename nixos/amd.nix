1413ebba24f40c708fc101994caaf128131832f7
Cainã Costa <me@cfcosta.com>
2023-09-22 13:39:24 -0300

    chore(amd): add scripts to change scaling governor

--- nixos/amd.nix
+++ nixos/amd.nix
@@ -3,10 +3,18 @@ with lib;
 let
   cfg = config.dusk;
   kernelVersion = config.boot.kernelPackages.kernel.version;
+  scripts = with pkgs; [
+    (writeShellScriptBin "amd-set-performance"
+      "echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor")
+    (writeShellScriptBin "amd-set-powersaving"
+      "echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor")
+  ];
 in {
   options.dusk.amd.enable = mkEnableOption "AMD Config and Optimizations";
 
   config = mkIf cfg.enable {
+    environment.systemPackages = scripts;
+
     # Enables the amd cpu scaling https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
     # On recent AMD CPUs this can be more energy efficient.
     boot = mkMerge [
