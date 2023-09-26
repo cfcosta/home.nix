eee946f27c31c1464911d890d4bcec2cc021a4b0
Cainã Costa <me@cfcosta.com>
2023-09-26 17:26:26 -0300

    fix(macos): load nix on ~/.profile

--- machines/drone.nix
+++ machines/drone.nix
@@ -17,4 +17,10 @@
       showBattery = true;
     };
   };
+
+  programs.bash.profileExtra = ''
+    if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+      . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+    fi
+  '';
 }
