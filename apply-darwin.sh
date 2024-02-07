2d282b9c4507278d2ccf6f112ac70cf6d5eb3301
Cainã Costa <me@cfcosta.com>
2024-02-07 14:06:55 -0300

    darwin: homebrew, some Mac App Store apps

--- apply-darwin.sh
+++ apply-darwin.sh
@@ -6,4 +6,16 @@ CMD="${1:-switch}"
 ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 NIX="nix --extra-experimental-features nix-command --extra-experimental-features flakes"
 
+if [[ "$(uname -s)" != "Darwin" ]]; then
+	echo "This is meant only for macOS hosts."
+	exit 1
+fi
+
+if [ ! -f "/opt/homebrew/bin/brew" ]; then
+	echo "Homebrew not found, installing it."
+	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	echo "Homebrew found, continuing it."
+fi
+
 ${NIX} run nix-darwin -- "$CMD" --flake "${ROOT}#$(hostname -s)"
--- darwin/default.nix
+++ darwin/default.nix
@@ -1,8 +1,43 @@
 { pkgs, lib, config, ... }:
 with lib; {
+  documentation = {
+    enable = true;
+    doc.enable = true;
+    info.enable = true;
+    man.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    bashInteractive
+    curl
+    file
+    git
+    wget
+  ];
+
+  homebrew = {
+    enable = true;
+
+    masApps = {
+      # "Amphetamine" = 37984704;
+      # "Bitwarden Password Manager" = 1137397744;
+      # "Fantastical Calendar" = 718043190;
+      # "Monodraw" = 920404675;
+      "Reeder 5" = 1529448980;
+      # "Tailscale" = 1470499037;
+      "Telegram" = 747648890;
+      # "Todoist: To-Do List & Planner" = 572688855;
+      "Whatsapp" = 310633997;
+    };
+
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+    };
+  };
+
   nix = {
     useDaemon = true;
-
     gc.automatic = true;
 
     settings = {
@@ -20,14 +55,7 @@ with lib; {
   };
 
   programs.bash.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    bashInteractive
-    curl
-    file
-    git
-    wget
-  ];
+  programs.gnupg.agent.enable = true;
 
   system.stateVersion = 4;
 }
