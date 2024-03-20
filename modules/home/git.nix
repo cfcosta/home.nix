591ac0a3a3708d2e9005a0cb82013c27d15e6962
Cainã Costa <me@cfcosta.com>
2024-03-20 11:29:54 -0300

    chore(home+git): add some git config flags

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -51,12 +51,21 @@ in {
       extraConfig = {
         blame.pager = "delta";
         commit.verbose = true;
+        diff.algorithm = "histogram";
         diff.colorMoved = "default";
+        filter.lfs.smudge = "git-lfs smudge -- %f";
         github.user = cfg.accounts.github;
+        help.autocorrect = 10;
         init.defaultBranch = cfg.git.defaultBranch;
         merge.conflictstyle = "diff3";
         pull.ff = "only";
         push.autoSetupRemote = true;
+        rebase = {
+          autosquash = true;
+          autostash = true;
+        };
+        rerere.enabled = true;
+        url."git@github.com:".insteadOf = "https://github.com/";
       };
     };
   };
