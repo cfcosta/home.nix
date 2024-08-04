d9642e92666da380572fdbbf4431f235d449dcf4
Cainã Costa <me@cfcosta.com>
2024-08-04 12:11:02 -0300

    feat(shell): use bat for coloring man pages

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -82,6 +82,7 @@ in
         EDITOR = "nvim";
         CDPATH = ".:${cfg.folders.code}";
         MANPAGER = "${pkgs.less}/bin/less -s -M +Gg";
+        MAN_PAGER = "sh -c 'col -bx | bat -l man -p'";
       };
 
       historySize = 1000000;
@@ -124,15 +125,6 @@ in
 
       bashrcExtra = ''
         ${if pkgs.stdenv.isDarwin then (builtins.readFile ./macos.sh) else ""}
-
-        # Man-pages coloring with Dracula theme
-        export LESS_TERMCAP_mb=$'\e[1;31m'      # begin bold
-        export LESS_TERMCAP_md=$'\e[1;34m'      # begin blink
-        export LESS_TERMCAP_so=$'\e[01;45;37m'  # begin reverse video
-        export LESS_TERMCAP_us=$'\e[01;36m'     # begin underline
-        export LESS_TERMCAP_me=$'\e[0m'         # reset bold/blink
-        export LESS_TERMCAP_se=$'\e[0m'         # reset reverse video
-        export LESS_TERMCAP_ue=$'\e[0m'         # reset underline
 
         [ -f "${cfg.shell.environmentFile}" ] && source "${cfg.shell.environmentFile}"
       '';
