ee1f6f4d3bff3ebc2013d36d1ff176fc501824f9
Cainã Costa <me@cfcosta.com>
2024-08-10 12:46:17 -0300

    chore(script): only require root for apply.sh when applying

--- apply.sh
+++ apply.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-[ "$(whoami)" == "root" ] || (
-	echo "This script needs to be run as root."
-	exit 1
-)
-
 CMD="${1:-switch}"
 ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+if [ "$(whoami)" == "root" ] && [ "${CMD}" != "build" ]; then
+	echo "This script needs to be run as root."
+	exit 1
+fi
 
 exec nixos-rebuild "${CMD}" --flake "${ROOT}#$(hostname)"
