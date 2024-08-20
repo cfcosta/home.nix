bf6042d07228c7d2afd11040fd3c22800ecb2cf4
Cainã Costa <me@cfcosta.com>
2024-08-20 11:36:27 -0300

    chore: fix pre-commit gitignore

--- modules/home/zed/default.nix
+++ modules/home/zed/default.nix
@@ -19,7 +19,6 @@ in
     };
 
     home.activation.setupZedConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      [ -f $HOME/.config/zed/settings.json ] && rm $HOME/.config/zed/settings.json
       cp -Lrf $HOME/.config/zed/settings.nix.json $HOME/.config/zed/settings.json
     '';
 
