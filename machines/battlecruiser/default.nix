8130056272e61f6980348cf35b0a7b39a98e1f4a
Cainã Costa <me@cfcosta.com>
2024-09-10 11:13:40 -0300

    chore(battlecruiser): join everything into a single file

--- machines/battlecruiser/default.nix
+++ machines/battlecruiser/default.nix
@@ -1,19 +1,45 @@
-{ ... }:
+{
+  pkgs,
+  modulesPath,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkDefault;
+in
 {
   imports = [
-    ./hardware.nix
-    ./refind.nix
+    "${modulesPath}/installer/scan/not-detected.nix"
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
 
-  networking.hostName = "battlecruiser";
+      extraModulePackages = [ ];
 
-  time.timeZone = "America/Sao_Paulo";
+      initrd = {
+        availableKernelModules = [
+          "nvme"
+          "ahci"
+          "thunderbolt"
+          "xhci_pci"
+          "usbhid"
+          "usb_storage"
+          "sd_mod"
+        ];
+        kernelModules = [ ];
+      };
+
+      kernelModules = [ "kvm-amd" ];
+
+      systemd-boot.enable = true;
+    };
+  };
 
   dusk = {
     enable = true;
+
     user = "cfcosta";
 
     cosmic.enable = true;
@@ -21,6 +47,59 @@
     privacy.enable = true;
     tailscale.enable = true;
     virtualisation.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    refind
+    efibootmgr
+  ];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/267a2e89-f17c-4ae8-ba84-709fda2a95aa";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/0B55-0450";
+      fsType = "vfat";
+    };
+  };
+
+  hardware = {
+    enableRedistributableFirmware = true;
+    cpu.amd.updateMicrocode = true;
+  };
+
+  networking = {
+    hostName = "battlecruiser";
+
+    useDHCP = mkDefault true;
+  };
+
+  time.timeZone = "America/Sao_Paulo";
+
+  swapDevices = [ ];
+
+  system.activationScripts = {
+    refind-install = {
+      deps = [ ];
+      text = ''
+        if [ -s /run/current-system/sw/bin/refind-install ];then
+          if [ ! -s /boot/EFI/refind/refind_x64.efi ]; then
+            OLDPATH="$PATH"
+            PATH="/run/current-system/sw/bin"
+            ${pkgs.refind}/bin/refind-install
+            PATH="$OLDPATH"
+            printf 'true' > /tmp/refind
+          else
+            printf 'installed/true' > /tmp/refind
+          fi
+        else
+          printf 'skip/true' > /tmp/refind
+        fi
+      '';
+    };
   };
 
   # Workaround fix for nm-online-service from stalling on Wireguard interface.
