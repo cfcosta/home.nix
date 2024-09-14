4a3673e9fec166017ea2aaa89d46d5b395e18ed4
Cainã Costa <me@cfcosta.com>
2024-09-14 14:38:06 -0300

    chore: remove hydra/iohk substituters

--- system/default.nix
+++ system/default.nix
@@ -11,19 +11,19 @@ let
   inherit (lib) mkForce;
 in
 {
-  assertions = [
-    {
-      assertion = flavor == "nixos" || flavor == "darwin";
-      message = "System flavor must be either 'nixos' or 'darwin'";
-    }
-  ];
-
   imports = [
     ../options.nix
     ../user.nix
   ];
 
   config = {
+    assertions = [
+      {
+        assertion = flavor == "nixos" || flavor == "darwin";
+        message = "System flavor must be either 'nixos' or 'darwin'";
+      }
+    ];
+
     documentation = {
       enable = true;
       doc.enable = true;
@@ -120,33 +120,11 @@ in
           "flakes"
         ];
 
-        extra-substituters = [
-          "https://cache.iog.io"
-          "https://hydra-node.cachix.org"
-          "https://cardano-scaling.cachix.org"
-        ];
-
-        extra-trusted-public-keys = [
-          "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-          "hydra-node.cachix.org-1:vK4mOEQDQKl9FTbq76NjOuNaRD4pZLxi1yri31HHmIw="
-          "cardano-scaling.cachix.org-1:RKvHKhGs/b6CBDqzKbDk0Rv6sod2kPSXLwPzcUQg9lY="
-        ];
-
         system-features = [
           "nixos-test"
           "benchmark"
           "big-parallel"
           "kvm"
-        ];
-
-        trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-        ];
-
-        substituters = [
-          "https://cache.nixos.org/"
-          "https://cache.iog.io/"
         ];
       };
     };
