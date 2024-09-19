c6f94c6a390582b4618f3dfb91326a5d9e017f87
Cainã Costa <me@cfcosta.com>
2024-09-19 14:05:46 -0300

    fix: override litellm to fix aider-chat

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,30 @@
-inputs: _: pkgs: {
-  agenix = inputs.agenix.packages.${pkgs.system}.default;
+inputs: _: final: {
+  agenix = inputs.agenix.packages.${final.system}.default;
 
-  catppuccin-refind = import ./refind { inherit inputs pkgs; };
-  waydroid-script = import ./waydroid { inherit inputs pkgs; };
+  catppuccin-refind = import ./refind {
+    inherit inputs;
+    pkgs = final;
+  };
+
+  python3Packages = final.python3Packages // {
+    litellm =
+      let
+        version = "v1.44.7";
+      in
+      final.litellm.overrideAttrs (_: {
+        inherit version;
+
+        src = final.fetchPypi {
+          inherit version;
+
+          pname = "litellm";
+          hash = "";
+        };
+      });
+  };
+
+  waydroid-script = import ./waydroid {
+    inherit inputs;
+    pkgs = final;
+  };
 }
