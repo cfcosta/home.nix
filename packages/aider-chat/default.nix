2f865b542ce1c24e0a134f766d501c32f7c51a44
Cainã Costa <me@cfcosta.com>
2024-09-20 14:31:25 -0300

    fix(aider-chat): enforce version selection for the litellm package

--- packages/aider-chat/default.nix
+++ packages/aider-chat/default.nix
@@ -8,15 +8,21 @@
 let
   python3 = python311.override {
     self = python3;
-    packageOverrides = _: super: { tree-sitter = super.tree-sitter_0_21; };
+    packageOverrides = _: super: {
+      inherit litellm;
+
+      tree-sitter = super.tree-sitter_0_21;
+    };
   };
 
   litellm =
     let
       version = "1.44.7";
     in
-    python3.pkgs.litellm.overrideAttrs (_: {
+    python311.pkgs.buildPythonApplication {
+      pname = "litellm";
       inherit version;
+      pyproject = true;
 
       src = python3.pkgs.fetchPypi {
         inherit version;
@@ -24,7 +30,7 @@ let
         pname = "litellm";
         hash = "sha256-yPj52ABlvoFYAlgXfzoAbehtLErx+acyrDe9MXoT8EI=";
       };
-    });
+    };
 in
 python3.pkgs.buildPythonApplication {
   inherit (aider-chat) meta version;
@@ -38,44 +44,7 @@ python3.pkgs.buildPythonApplication {
 
   build-system = with python3.pkgs; [ setuptools-scm ];
 
-  dependencies =
-    [ litellm ]
-    ++ (with python3.pkgs; [
-      aiohappyeyeballs
-      backoff
-      beautifulsoup4
-      configargparse
-      diff-match-patch
-      diskcache
-      flake8
-      gitpython
-      grep-ast
-      importlib-resources
-      jiter
-      json5
-      jsonschema
-      networkx
-      numpy
-      packaging
-      pathspec
-      pexpect
-      pillow
-      playwright
-      prompt-toolkit
-      ptyprocess
-      pypager
-      pypandoc
-      pyperclip
-      pyyaml
-      rich
-      scipy
-      sounddevice
-      soundfile
-      streamlit
-      tokenizers
-      watchdog
-      psutil
-    ]);
+  dependencies = [ litellm ];
 
   buildInputs = [ portaudio ];
 
