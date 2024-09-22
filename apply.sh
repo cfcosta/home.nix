0a4406d9c2e29a74e3119933da9a62004ffcd85c
Cainã Costa <me@cfcosta.com>
2024-09-22 15:06:41 -0300

    feat(apply.sh): XCode command-line tools, better logging

--- apply.sh
+++ apply.sh
@@ -8,11 +8,31 @@ HOSTNAME="$(hostname -s)"
 
 ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+# shellcheck source=/dev/null
 . "${ROOT}/scripts/bash-lib.sh"
 
 if [ ! -f "$ROOT/machines/$HOSTNAME.nix" ]; then
 	_fatal 'you must define a machine with this hostname on the "machines" folder'
 fi
+
+setup_darwin_xcode_cli_tools() {
+
+	if xcode-select -p &>/dev/null; then
+		_info "Found command-line tools at $(xcode-select -p)}"
+		return 0
+	fi
+
+	_warn "Xcode Command Line Tools are not installed, installing..."
+
+	xcode-select --install
+
+	_info "Please follow the prompts to install Xcode Command Line Tools."
+	_info "After installation is complete, please run this script again."
+
+	if ! xcode-select -p &>/dev/null; then
+		_fatal "Even after install, could not find installed command line tools, please try again"
+	fi
+}
 
 setup_darwin_xcode_license() {
 	XCODE_VERSION="$(xcodebuild -version | grep '^Xcode\s' | sed -E 's/^Xcode[[:space:]]+([0-9\.]+)/\1/')"
@@ -34,6 +54,7 @@ setup_darwin_xcode_license() {
 }
 
 setup_darwin() {
+	setup_darwin_xcode_cli_tools
 	setup_darwin_xcode_license
 
 	NIX_ROOT="/run/current-system/sw"
@@ -41,25 +62,38 @@ setup_darwin() {
 	export PATH="${NIX_ROOT}/bin:$PATH"
 
 	if ! which nix &>/dev/null; then
-		_info "Nix not found, installing."
+		_warn "Nix not found, installing."
 
 		curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 
-		_info "Nix installed."
+		_info "Done, Nix installed successfully."
+	fi
+
+	if which nix &>/dev/null; then
+		_info "Found Nix: $(which nix)"
+	else
+		_fatal "Could not find Nix even after install!"
 	fi
 
 	# Make sure we are connected to the Nix Daemon
-	#
 	# shellcheck source=/dev/null
-	[ -e "${NIX_ROOT}/etc/profile.d/nix-daemon.sh" ] && . "${NIX_ROOT}/etc/profile.d/nix-daemon.sh"
+	if [ -e "${NIX_ROOT}/etc/profile.d/nix-daemon.sh" ]; then
+		_info "Found Nix Daemon script, loading..."
 
-	if [ ! -f /opt/homebrew/bin/brew ]; then
-		_info "Homebrew not found, installing..."
-		NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-		_info "Homebrew installed."
+		. "${NIX_ROOT}/etc/profile.d/nix-daemon.sh"
 	fi
+
+	BREW_PATH=/opt/homebrew/bin/brew
+
+	if [ ! -e "${BREW_PATH}" ]; then
+		_warn "Homebrew not found, installing..."
+		NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+
+	_info "Homebrew found: ${BREW_PATH}"
 }
+
+_info "Found $(uname -s) machine, setting up environment."
 
 case "$(uname -s)" in
 "Darwin")
@@ -69,18 +103,26 @@ case "$(uname -s)" in
 
 	setup_darwin
 
-	exec ${NIX} run nix-darwin -- "$CMD" --flake "${ROOT}#${HOSTNAME}"
+	CMD="${NIX} run nix-darwin -- $CMD --flake ${ROOT}#${HOSTNAME}"
+
+	_info "Running command: $(_blue "${CMD}")"
+
+	exec ${CMD}
 	;;
 "Linux")
 	if [ "$(whoami)" != "root" ] && [ "${CMD}" == "switch" ]; then
 		_fatal "This script needs to be run as root."
 	fi
 
-	exec nixos-rebuild "${CMD}" --flake "${ROOT}#${HOSTNAME}"
+	CMD="${NIX} nixos-rebuild $CMD --flake ${ROOT}#${HOSTNAME}"
 	;;
 *)
 	_fatal "Invalid system"
 	;;
 esac
+
+_info "Running command: $(_blue "${CMD}")"
+
+${CMD}
 
 _info "Done!"
