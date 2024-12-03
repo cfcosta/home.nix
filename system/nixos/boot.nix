cc6ae82672878c72e7814d0f6ecbed549957984e
Cainã Costa <me@cfcosta.com>
2024-12-03 12:09:33 -0300

    nixos: force installing refind theme

--- system/nixos/boot.nix
+++ system/nixos/boot.nix
@@ -35,10 +35,27 @@ in
 
     system.activationScripts.refind-install = {
       text = ''
-        if [ -s /run/current-system/sw/bin/refind-install ];then
+        if [ -s /run/current-system/sw/bin/refind-install ]; then
           OLDPATH="$PATH"
           PATH="/run/current-system/sw/bin"
+          # Install rEFInd
           ${pkgs.dusk.refind}/bin/refind-install
+          
+          # Copy theme files to EFI partition
+          esp_mount=$(findmnt -n -o TARGET /boot/efi || findmnt -n -o TARGET /boot)
+          themes_dir="$esp_mount/EFI/refind/themes"
+          mkdir -p "$themes_dir"
+          cp -r ${pkgs.dusk.refind}/share/refind/themes/* "$themes_dir/"
+          
+          # Configure refind.conf to use the theme
+          refind_conf="$esp_mount/EFI/refind/refind.conf"
+          if [ -f "$refind_conf" ]; then
+            # Remove any existing include theme.conf lines
+            sed -i '/^include themes\//d' "$refind_conf"
+            # Add new theme include
+            echo "include themes/catppuccin/theme.conf" >> "$refind_conf"
+          fi
+          
           PATH="$OLDPATH"
           printf 'true' > /tmp/refind
         else
