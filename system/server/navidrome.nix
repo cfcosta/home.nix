943c6b65ea15d67360ac04ce62a73747e12c14c2
Cainã Costa <me@cfcosta.com>
2024-12-03 14:15:09 -0300

    temp: use different folder for navidrome

---
+++ system/server/navidrome.nix
@@ -0,0 +1,48 @@
+{ dusk-lib, ... }:
+dusk-lib.defineService rec {
+  name = "navidrome";
+  port = 4533;
+  config =
+    { config, ... }:
+    {
+      config.services.navidrome = {
+        inherit (config.dusk.system.server.${name}) enable;
+
+        user = config.dusk.username;
+        group = "users";
+
+        openFirewall = false;
+
+        settings = {
+          MusicFolder = "/media/Music2";
+          # Server settings
+          Address = "0.0.0.0";
+          Port = port;
+          BaseUrl = "https://${name}.${config.dusk.system.server.domain}";
+          UILoginBackground = "";
+
+          # Scanning settings
+          ScanSchedule = "@every 10m";
+          AutoImportPlaylists = true;
+
+          # Library settings
+          DefaultTheme = "Dark";
+          DefaultLanguage = "en";
+          EnableGravatar = true;
+          EnableStarRating = true;
+          EnableFavourites = true;
+
+          # Playback settings
+          EnableDownloads = true;
+          EnableCoverAnimation = false;
+
+          # Security settings
+          AuthRequestLimit = 5;
+          SessionTimeout = "24h";
+          EnableSharing = true;
+
+          ArtistArtPriority = "artist.*, album/artist.*, external";
+        };
+      };
+    };
+}
