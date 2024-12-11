51b6cfdd4dcd59c4952b11f0461cd7c670f9fe31
Cainã Costa <me@cfcosta.com>
2024-12-11 12:57:17 -0300

    feat(hyprland): theme, keybindings, yofi
    The code introduces support for the Hyprland window manager, incorporating the Catppuccin theme and configuring keybindings to interact with various applications, such as the terminal, browser, and yofi. Key modifications include adding `catppuccin-hyprland` to `flake.lock` and `flake.nix`, and updating `default.nix` to enable Hyprland, set environment variables, and configure the window manager's settings, including the theme, cursor, and keybindings. This setup also enables `hyprlock`, `hypridle`, and `hyprpaper` services, and sets `grimblast` as a system package. The keybindings support various actions, including running applications, switching workspaces, and capturing the screen.

--- system/nixos/hyprland/default.nix
+++ system/nixos/hyprland/default.nix
@@ -10,13 +10,53 @@ let
 in
 {
   config = mkIf cfg.enable {
-    environment.sessionVariables = {
-      BROWSER = config.dusk.defaults.browser;
-      TERMINAL = config.dusk.defaults.terminal;
-      QT_QPA_PLATFORM = "wayland";
+    environment = {
+      sessionVariables = {
+        BROWSER = config.dusk.defaults.browser;
+        TERMINAL = config.dusk.defaults.terminal;
+        QT_QPA_PLATFORM = "wayland";
+      };
+
+      systemPackages = with pkgs; [
+        grimblast
+      ];
     };
 
     home-manager.users.${config.dusk.username} = {
+      home.pointerCursor = {
+        gtk.enable = true;
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Classic";
+        size = 16;
+      };
+
+      gtk.enable = true;
+
+      programs.hyprlock.enable = true;
+
+      services = {
+        hypridle.enable = true;
+        hyprpaper.enable = true;
+      };
+
+      wayland.windowManager.hyprland = {
+        enable = true;
+
+        settings = {
+          "$mod" = "SUPER";
+
+          source = [ "${inputs.catppuccin-hyprland}/themes/mocha.conf" ];
+
+          bind = [
+            "$mod, Space, exec, ${pkgs.yofi}/bin/yofi apps"
+            "$mod, Enter, exec, ${config.dusk.defaults.terminal}"
+            "$mod, B, exec, ${config.dusk.defaults.browser}"
+            ", Print, exec, grimblast copy area"
+          ] ++ keybindings.switch-workspace;
+        };
+
+        systemd.variables = [ "--all" ];
+      };
     };
 
     programs.hyprland = {
