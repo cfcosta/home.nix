edd6286a336c3f73e81015dbbe72a66724de675f
Lucas Barros <lucasnbarros@gmail.com>
2024-12-16 17:10:29 -0300

    update machine

--- flake.nix
+++ flake.nix
@@ -211,6 +211,9 @@
     // {
       darwinConfigurations.drone = buildDarwin "drone";
       darwinConfigurations.firebat = buildDarwin "firebat";
-      nixosConfigurations.battlecruiser = buildNixos "battlecruiser";
+      nixosConfigurations = {
+        battlecruiser = buildNixos "battlecruiser";
+        terminus = buildNixos "terminus";
+      };
     };
 }
