9eeb76a2501a807145d234dba5abb4819436d587
Cainã Costa <me@cfcosta.com>
2024-12-17 12:34:34 -0300

    chore(gnome): remove extra GTK config

--- system/nixos/gnome/default.nix
+++ system/nixos/gnome/default.nix
@@ -39,18 +39,6 @@ in
           name = "Adwaita";
           package = pkgs.adwaita-icon-theme;
         };
-
-        gtk3.extraConfig = {
-          Settings = ''
-            gtk-application-prefer-dark-theme=1
-          '';
-        };
-
-        gtk4.extraConfig = {
-          Settings = ''
-            gtk-application-prefer-dark-theme=1
-          '';
-        };
       };
     };
 
