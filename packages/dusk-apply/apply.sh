eb0eb114442959b51862fb16c7b1cfbf2f58a3df
Cainã Costa <me@cfcosta.com>
2024-12-30 12:53:11 -0300

    chore(apply): use dusk-system-verify from nix always

--- packages/dusk-apply/apply.sh
+++ packages/dusk-apply/apply.sh
@@ -13,6 +13,7 @@ _info "Running action $(_red "\`${ACTION}\`") for $(_blue "$(uname -s)") machine
 if [ -d "${ROOT}" ]; then
   _info "Found project root: $(_blue "${ROOT}")"
 fi
+
 if [ ! -f "$ROOT/machines/$HOSTNAME.nix" ]; then
   _error "Could not find a machine definition for $(_blue "${HOSTNAME}")."
   _info "Please create a file for it on the following path: $(_red "$ROOT/machines/$HOSTNAME.nix") (and ensure it has been added to git working copy, with $(_green "git add --intent-to-add") or something similar)."
@@ -23,15 +24,14 @@ if [ "$(whoami)" == "root" ]; then
   _fatal "This script must be run as a normal user. Sudo password will be asked from you when required."
 fi
 
+# shellcheck disable=SC2086
+_run ${NIX} run "${ROOT}#dusk-system-verify"
+
 case "$(uname -s)" in
 "Darwin")
-  dusk-system-verify || _fatal "System failed minimum requirements to run"
-
   CMD="${NIX} run nix-darwin -- $ACTION --flake ${ROOT}#${HOSTNAME} -L"
   ;;
 "Linux")
-  dusk-system-verify || _fatal "System failed minimum requirements to run"
-
   CMD="nixos-rebuild $ACTION --flake ${ROOT}#${HOSTNAME} -L"
 
   case "$ACTION" in
