63f16de047df8a191834ca5d418775846d4cc056
Cainã Costa <me@cfcosta.com>
2025-01-02 10:56:05 -0300

    feat: zellij

--- system/shell/default.nix
+++ system/shell/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./starship
     ./tmux
+    ./zellij
   ];
 }
---
+++ system/shell/zellij/default.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkOption mkIf types;
+  cfg = config.dusk.shell.zellij;
+in
+{
+  options.dusk.shell.zellij.enable = mkOption {
+    type = types.bool;
+    default = true;
+    description = "Whether or not to enable the Zellij terminal multiplexer";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ zellij ];
+
+    home-manager.users.${config.dusk.username} = _: {
+      programs.zellij = {
+        enable = true;
+        enableBashIntegration = true;
+        settings.pane_frames = false;
+      };
+    };
+  };
+}
