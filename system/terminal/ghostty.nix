b5ca12a02e0bf71983e09a4c6a43e5b7512323a4
Cainã Costa <me@cfcosta.com>
2025-02-09 17:38:48 -0300

    feat(ghostty): add a CRT filter

--- system/terminal/ghostty.nix
+++ system/terminal/ghostty.nix
@@ -15,6 +15,31 @@ let
   cfg = config.dusk.terminal;
 
   window-theme = if (flavor == "nixos") then "ghostty" else "system";
+  crt-filter = ''
+    float warp = 0.0; // simulate curvature of CRT monitor
+    float scan = 0.50; // simulate darkness between scanlines
+
+    void mainImage(out vec4 fragColor, in vec2 fragCoord)
+    {
+        // squared distance from center
+        vec2 uv = fragCoord / iResolution.xy;
+        vec2 dc = abs(0.5 - uv);
+        dc *= dc;
+        
+        // warp the fragment coordinates
+        uv.x -= 0.5; uv.x *= 1.0 + (dc.y * (0.3 * warp)); uv.x += 0.5;
+        uv.y -= 0.5; uv.y *= 1.0 + (dc.x * (0.4 * warp)); uv.y += 0.5;
+
+        // determine if we are drawing in a scanline
+        float apply = abs(sin(fragCoord.y) * 0.25 * scan);
+            
+        // sample the texture
+        vec3 color = texture(iChannel0, uv).rgb;
+
+        // mix the sampled color with the scanline intensity
+        fragColor = vec4(mix(color, vec3(0.0), apply), 1.0);
+    }
+  '';
 in
 {
   imports =
@@ -49,6 +74,7 @@ in
         window-decoration = false
         window-theme = ${window-theme}
         macos-titlebar-style = hidden
+        custom-shader = ${pkgs.writeText "crt.glsl" crt-filter}
       '';
     };
   };
