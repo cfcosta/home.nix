eaea4cbc04d577fccb7861e5e4e295e80dd11460
Cainã Costa <me@cfcosta.com>
2025-03-29 13:02:15 -0300

    feat(gnome): set hotkeys to change workspaces

--- system/nixos/gnome/default.nix
+++ system/nixos/gnome/default.nix
@@ -29,6 +29,51 @@ in
           settings = {
             "org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
+            "org/gnome/desktop/wm/keybindings" = {
+              close = [
+                "<Super>q"
+              ];
+
+              maximize = [ ];
+              minimize = [ ];
+
+              move-to-monitor-down = [ ];
+              move-to-monitor-left = [ ];
+              move-to-monitor-right = [ ];
+              move-to-monitor-up = [ ];
+
+              move-to-workspace-1 = [ "<Shift><Super>1" ];
+              move-to-workspace-2 = [ "<Shift><Super>2" ];
+              move-to-workspace-3 = [ "<Shift><Super>3" ];
+              move-to-workspace-4 = [ "<Shift><Super>4" ];
+              move-to-workspace-5 = [ "<Shift><Super>5" ];
+              move-to-workspace-6 = [ "<Shift><Super>6" ];
+              move-to-workspace-7 = [ "<Shift><Super>7" ];
+              move-to-workspace-8 = [ "<Shift><Super>8" ];
+              move-to-workspace-9 = [ "<Shift><Super>9" ];
+
+              move-to-workspace-down = [ ];
+              move-to-workspace-up = [ ];
+
+              switch-to-workspace-1 = [ "<Super>1" ];
+              switch-to-workspace-2 = [ "<Super>2" ];
+              switch-to-workspace-3 = [ "<Super>3" ];
+              switch-to-workspace-4 = [ "<Super>4" ];
+              switch-to-workspace-5 = [ "<Super>5" ];
+              switch-to-workspace-6 = [ "<Super>6" ];
+              switch-to-workspace-7 = [ "<Super>7" ];
+              switch-to-workspace-8 = [ "<Super>8" ];
+              switch-to-workspace-9 = [ "<Super>9" ];
+
+              switch-to-workspace-down = [ ];
+              switch-to-workspace-left = [ ];
+              switch-to-workspace-right = [ ];
+              switch-to-workspace-up = [ ];
+
+              toggle-maximized = [ ];
+              unmaximize = [ ];
+            };
+
             "org/gnome/shell" = {
               disable-user-extensions = false;
               enabled-extensions = with pkgs.gnomeExtensions; [
