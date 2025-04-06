b7dd293e99b8f01e54abbd4996cef63f3c5e9272
Cainã Costa <me@cfcosta.com>
2025-04-06 20:55:05 -0300

    fix(ghostty): properly set config file on both darwin and nixos

--- system/terminal.nix
+++ system/terminal.nix
@@ -9,7 +9,16 @@ let
   inherit (lib) mkOption types;
   cfg = config.dusk.terminal;
 
-  window-theme = if (flavor == "nixos") then "ghostty" else "system";
+  configFile = ''
+    theme = ${cfg.theme}
+    window-theme = ${if (flavor == "nixos") then "ghostty" else "system"}
+
+    background-opacity = 0.8
+    background-blur = true
+
+    font-family = ${cfg.font-family}
+    font-size = ${toString cfg.font-size}
+  '';
 in
 {
   options.dusk.terminal = {
@@ -37,39 +46,33 @@ in
   };
 
   config =
-    {
-      home-manager.users.${config.dusk.username}.xdg.configFile."ghostty/config".text = ''
-        theme = ${cfg.theme}
-        window-theme = ${window-theme}
+    if (flavor == "nixos") then
+      {
+        environment.systemPackages = [ pkgs.ghostty ];
 
-        background-opacity = 0.8
-        background-blur = true
-
-        font-family = ${cfg.font-family}
-        font-size = ${toString cfg.font-size}
-      '';
-    }
-    // (
-      if (flavor == "nixos") then
-        {
-          environment.systemPackages = [ pkgs.ghostty ];
-
-          home-manager.users.${config.dusk.username}.programs.bash.initExtra = ''
+        home-manager.users.${config.dusk.username} = {
+          programs.bash.initExtra = ''
             . ${pkgs.ghostty}/share/ghostty/shell-integration/bash/ghostty.bash
           '';
-        }
-      else
-        {
-          homebrew = {
-            enable = true;
-            casks = [ "ghostty" ];
-          };
 
-          home-manager.users.${config.dusk.username}.programs.bash.initExtra = ''
+          xdg.configFile."ghostty/config".text = configFile;
+        };
+      }
+    else
+      {
+        homebrew = {
+          enable = true;
+          casks = [ "ghostty" ];
+        };
+
+        home-manager.users.${config.dusk.username} = {
+          programs.bash.initExtra = ''
             if [ -n "''${GHOSTTY_RESOURCES_DIR}" ]; then
               builtin source "''${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash"
             fi
           '';
-        }
-    );
+
+          xdg.configFile."ghostty/config".text = configFile;
+        };
+      };
 }
