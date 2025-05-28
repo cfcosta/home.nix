1d2f99685ad4008e13a90cf23704d700fa84b2ed
Cainã Costa <me@cfcosta.com>
2025-05-28 11:26:30 -0300

    feat(jujutsu): add jjui and radicle

--- system/home/jujutsu.nix
+++ system/home/jujutsu.nix
@@ -15,17 +15,24 @@ let
 in
 {
   config = {
-    home.file.".config/rustfmt/rustfmt.toml".text = ''
-      edition = "2024"
+    home = {
+      file.".config/rustfmt/rustfmt.toml".text = ''
+        edition = "2024"
 
-      reorder_imports = true
-      imports_granularity = "Crate"
-      imports_layout = "HorizontalVertical"
-      max_width = 102
-      group_imports = "StdExternalCrate"
-      trailing_comma = "Vertical"
-      trailing_semicolon = true
-    '';
+        reorder_imports = true
+        imports_granularity = "Crate"
+        imports_layout = "HorizontalVertical"
+        max_width = 102
+        group_imports = "StdExternalCrate"
+        trailing_comma = "Vertical"
+        trailing_semicolon = true
+      '';
+
+      packages = with pkgs; [
+        jjui
+        radicle-node
+      ];
+    };
 
     programs.jujutsu = {
       enable = true;
