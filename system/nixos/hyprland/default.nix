f323cddbfd56876b2300b48521146f1204f54fc4
Cainã Costa <me@cfcosta.com>
2025-07-08 00:01:12 -0300

    chore(hyprland): use animations from omarchy

--- system/nixos/hyprland/default.nix
+++ system/nixos/hyprland/default.nix
@@ -188,13 +188,28 @@ in
 
           animations = {
             enabled = true;
-            bezier = "overshot, 0.05, 0.9, 0.1, 1.1";
+            bezier = [
+              "easeOutQuint,0.23,1,0.32,1"
+              "easeInOutCubic,0.65,0.05,0.36,1"
+              "linear,0,0,1,1"
+              "almostLinear,0.5,0.5,0.75,1.0"
+              "quick,0.15,0,0.1,1"
+            ];
             animation = [
-              "windows, 1, 1.5, overshot"
-              "windowsOut, 1, 1.5, overshot, popin 80%"
-              "border, 1, 10, overshot"
-              "fade, 1, 7, overshot"
-              "workspaces, 1, 1.5, overshot"
+              "global, 1, 10, default"
+              "border, 1, 5.39, easeOutQuint"
+              "windows, 1, 4.79, easeOutQuint"
+              "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+              "windowsOut, 1, 1.49, linear, popin 87%"
+              "fadeIn, 1, 1.73, almostLinear"
+              "fadeOut, 1, 1.46, almostLinear"
+              "fade, 1, 3.03, quick"
+              "layers, 1, 3.81, easeOutQuint"
+              "layersIn, 1, 4, easeOutQuint, fade"
+              "layersOut, 1, 1.5, linear, fade"
+              "fadeLayersIn, 1, 1.79, almostLinear"
+              "fadeLayersOut, 1, 1.39, almostLinear"
+              "workspaces, 0, 0, ease"
             ];
           };
 
