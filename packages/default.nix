c4747bee5d31d9a8a2ea2863ff6e76cc0d06f05b
Cainã Costa <me@cfcosta.com>
2025-10-31 11:13:19 -0300

    fix(nix): clean up warnings for `pkgs.system` being deprecated

--- packages/default.nix
+++ packages/default.nix
@@ -1,10 +1,14 @@
-inputs: _: super: {
+inputs: _: super:
+let
+  inherit (super.stdenv.hostPlatform) system;
+in
+{
   dusk-apply = super.callPackage ./dusk-apply { };
   dusk-keymap-switch = super.callPackage ./dusk-keymap-switch { };
   dusk-stdlib = super.callPackage ./dusk-stdlib { };
   dusk-system-verify = super.callPackage ./dusk-system-verify { };
   dusk-treefmt = super.callPackage ./treefmt.nix { inherit inputs; };
-  hypr-recorder = inputs.hypr-recorder.packages.${super.system}.default;
-  nightvim = inputs.neovim.packages.${super.system}.default;
-  nm-wifi = inputs.nm-wifi.packages.${super.system}.default;
+  hypr-recorder = inputs.hypr-recorder.packages.${system}.default;
+  nightvim = inputs.neovim.packages.${system}.default;
+  nm-wifi = inputs.nm-wifi.packages.${system}.default;
 }
