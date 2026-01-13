7d507aeb806e65fa520e3c6ee1e046abe4d183c2
Cainã Costa <me@cfcosta.com>
2026-01-13 17:19:58 -0300

    chore: open archives with xarchiver

--- system/hyprland/file-manager.nix
+++ system/hyprland/file-manager.nix
@@ -4,13 +4,42 @@
     environment.systemPackages = with pkgs; [
       pcmanfm
       tumbler
+      xarchiver
     ];
 
     services.tumbler.enable = true;
 
     home-manager.users.${config.dusk.username} = _: {
-      # Open directories with PCManFM
-      xdg.mimeApps.defaultApplications."inode/directory" = "pcmanfm.desktop";
+      xdg.mimeApps.defaultApplications = {
+        # Open directories with PCManFM
+        "inode/directory" = "pcmanfm.desktop";
+
+        # Open archive files with XArchiver
+        "application/gzip" = "xarchiver.desktop";
+        "application/java-archive" = "xarchiver.desktop";
+        "application/vnd.android.package-archive" = "xarchiver.desktop";
+        "application/vnd.rar" = "xarchiver.desktop";
+        "application/x-7z-compressed" = "xarchiver.desktop";
+        "application/x-bzip-compressed-tar" = "xarchiver.desktop";
+        "application/x-bzip2" = "xarchiver.desktop";
+        "application/x-compressed-tar" = "xarchiver.desktop";
+        "application/x-gtar" = "xarchiver.desktop";
+        "application/x-gzip" = "xarchiver.desktop";
+        "application/x-lzma" = "xarchiver.desktop";
+        "application/x-lzma-compressed-tar" = "xarchiver.desktop";
+        "application/x-rar" = "xarchiver.desktop";
+        "application/x-rar-compressed" = "xarchiver.desktop";
+        "application/x-tar" = "xarchiver.desktop";
+        "application/x-tgz" = "xarchiver.desktop";
+        "application/x-xz" = "xarchiver.desktop";
+        "application/x-xz-compressed-tar" = "xarchiver.desktop";
+        "application/x-zip" = "xarchiver.desktop";
+        "application/x-zip-compressed" = "xarchiver.desktop";
+        "application/x-zstd" = "xarchiver.desktop";
+        "application/x-zstd-compressed-tar" = "xarchiver.desktop";
+        "application/zip" = "xarchiver.desktop";
+        "application/zstd" = "xarchiver.desktop";
+      };
     };
   };
 }
