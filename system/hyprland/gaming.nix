f842c3d3dc70ffad9770d8597223d60cad6d6eeb
Cainã Costa <me@cfcosta.com>
2026-02-04 22:36:01 -0300

    chore: update flake

--- system/hyprland/gaming.nix
+++ system/hyprland/gaming.nix
@@ -8,21 +8,6 @@ let
   inherit (lib) mkIf;
 
   cfg = config.dusk.system.nixos.desktop.gaming;
-  xone-firmware = pkgs.xow_dongle-firmware.overrideAttrs (_old: rec {
-    name = "xone-firmware";
-    version = "045e_02e6";
-    src = pkgs.fetchurl {
-      url = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/driver/drvs/2015/12/20810869_8ce2975a7fbaa06bcfb0d8762a6275a1cf7c1dd3.cab";
-      sha256 = "sha256-5jiKJ6dXVpIN5zryRo461V16/vWavDoLUICU4JHRnwg=";
-    };
-    unpackCmd = ''
-      cabextract -F FW_ACC_00U.bin ${src}
-    '';
-    installPhase = ''
-      mkdir -p $out/lib/firmware
-      cp xow_dongle_045e_02e6.bin $out/lib/firmware/xone_dongle_02e6.bin
-    '';
-  });
   hytale = pkgs.fetchurl {
     url = "https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak";
     sha256 = "sha256-4DUP/hHgpbKvjRIF2ksxhtGFkvA9QSVOT4w+c4MXa+4=";
@@ -36,7 +21,6 @@ in
       heroic
       mangohud
       moonlight-qt
-      xone-firmware
     ];
 
     hardware = {
@@ -45,8 +29,6 @@ in
       # Support for Xbox One/Series X Controllers
       xone.enable = true;
       xpadneo.enable = false;
-
-      firmware = [ xone-firmware ];
     };
 
     home-manager.users.${config.dusk.username}.xdg.configFile."gamemode.ini".text = ''
