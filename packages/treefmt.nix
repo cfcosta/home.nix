cb269ce243ea9d9e5a7144f96eea787d766948a8
Cainã Costa <me@cfcosta.com>
2026-03-07 17:32:44 -0300

    chore(treefmt): add rustfmt options

--- packages/treefmt.nix
+++ packages/treefmt.nix
@@ -34,6 +34,10 @@ let
 
       formatter = {
         nixfmt.options = [ "--strict" ];
+        rustfmt.options = [
+          "--config"
+          "reorder_imports=true,imports_granularity=Crate,imports_layout=HorizontalVertical,max_width=80,group_imports=StdExternalCrate,trailing_comma=Vertical,trailing_semicolon=true"
+        ];
         shfmt.options = [
           "--ln"
           "bash"
