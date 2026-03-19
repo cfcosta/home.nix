74e3aba53a7d562380b110c8912bdcd9a2f661c7
Cainã Costa <me@cfcosta.com>
2026-03-19 17:16:10 -0300

    fix: cleanup swaync workaround

--- system/hyprland/swaync.nix
+++ system/hyprland/swaync.nix
@@ -1,17 +1,6 @@
-{ config, lib, ... }:
-let
-  inherit (lib) optionals;
-  nvidiaEnabled = config.dusk.system.nixos.nvidia.enable;
-in
+{ config, ... }:
 {
   home-manager.users.${config.dusk.username} = {
     services.swaync.enable = true;
-
-    # Fix for https://github.com/ErikReider/SwayNotificationCenter/issues/619
-    # Makes swaync hang after a single notification has been sent, only on Nvidia.
-    systemd.user.services.swaync.Service.Environment = optionals nvidiaEnabled [
-      "GSK_RENDERER=gl"
-      "GTK_DISABLE_VULKAN=1"
-    ];
   };
 }
